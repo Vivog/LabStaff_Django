8b6b12ac60e8607d95ebc982a511715f106dfd00
VIVOG <vivog2017@gmail.com>
2022-05-28 18:06:35 +0300

    added tempales of html page

--- labstaff/main_app/views.py
+++ labstaff/main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main_app/main.html')
+    return render(request, 'main_app/index.html')
 
 # Create your views here.
